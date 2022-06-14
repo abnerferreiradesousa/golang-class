6bb5f4b3779b7ece6f0d09ac2945535264e60770
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-13 21:13:10 -0300

    [feat]: second exercise

--- index.go
+++ index.go
@@ -8,3 +8,21 @@ func main() {
 	fmt.Printf("%b\n", a)
 	fmt.Printf("%x", a)
 }
+
+// Escreva expressões utilizando os operadores, e atribua seus valores a variáveis.
+
+import "fmt"
+
+func main() {
+	a := 29
+	b := 17
+
+	z := a < b
+	y := a > b
+	u := a >= b
+	v := a <= b
+	x := a != b
+	p := a == b
+
+	fmt.Println(p, x, v, u, y, z)
+}
