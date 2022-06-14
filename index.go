b3b2e7919ce2320e9eb3434815a7dbd31152c3ee
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-13 21:05:15 -0300

    [feat]: first exercise

--- index.go
+++ index.go
@@ -1,5 +1,10 @@
+// Escreva um programa que mostre um número em decimal, binário e hexadecimal.
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	a := 50
+	fmt.Printf("%d, %#x, %b\n", a, a, a)
+	fmt.Printf("%b\n", a)
+	fmt.Printf("%x", a)
 }
