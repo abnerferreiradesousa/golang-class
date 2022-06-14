7d99e6fcc7abb882dfba6efaff1cf3a489bb73c7
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-13 21:29:56 -0300

    [feat]: third and fourd exercise

--- index.go
+++ index.go
@@ -26,3 +26,18 @@ func main() {
 
 	fmt.Println(p, x, v, u, y, z)
 }
+
+// Crie constantes tipadas e não-tipadas.
+
+import "fmt"
+
+const a int = 10
+const b = 11
+
+
+func main() {
+	// Será que não estou redeclarando a variável b?
+	b := "Olá amor"
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T", b)
+}
