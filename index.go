969c1c83cfc242165d4a2dcb087c66050e033096
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-14 18:57:09 -0300

    [feat]: add exercise 5

--- index.go
+++ index.go
@@ -41,3 +41,20 @@ func main() {
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T", b)
 }
+
+// Utilizando iota, crie 4 constantes cujos valores sejam os próximos 4 anos.
+
+import "fmt"
+
+const (
+	_ = iota
+	x = iota + 2022
+	z
+	y
+	f
+)
+
+func main() {
+
+	fmt.Println(x, z, y, f)
+}
