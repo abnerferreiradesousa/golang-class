c5dec4af04253a0c3b83636d2dacebe321927300
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-06-15 21:09:27 -0300

    [feat]: add exercise loop

--- index.go
+++ index.go
@@ -58,3 +58,29 @@ func main() {
 
 	fmt.Println(x, z, y, f)
 }
+
+//Põe na tela: O unicode code point de todas as letras maiúsculas do alfabeto, três vezes cada.
+// Por exemplo:
+  //  65
+  //      U+0041 'A'
+  //      U+0041 'A'
+  //      U+0041 'A'
+  //  66
+  //      U+0042 'B'
+  //      U+0042 'B'
+  //      U+0042 'B' 
+  //  ...e por aí vai.
+
+	import "fmt"
+
+	func main() {
+		x := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	
+		for i := 0; i < len(x); i++ {
+			for index := 1; index <= 3; index++ {
+				fmt.Printf("%#U\n", x[i])
+			}
+	
+		}
+	
+	}
