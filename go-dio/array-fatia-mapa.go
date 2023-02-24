1f033e46d7f351de92cebed99691599ae7b6d75e
Abner <abnerferreira84@gmail.com>
2023-02-24 10:46:09 -0300

    feat: reorganizando pastas de estudos de Go.

---
+++ go-dio/array-fatia-mapa.go
@@ -0,0 +1,14 @@
+// Mapas - podem ser chamados também de arrays associativos, tabelas hash ou dicionários.
+
+// Abstração - esconder as implementações da ferramenta, não sendo nescessário entender os
+// detalhes, mas sim como usar a ferramenta.
+
+//  O que é tempo de execução e tempo de compilação?
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print()
+}
